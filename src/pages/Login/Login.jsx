6cfbf8b4232af51c26168bce9e6f9eb46e7db01d
Anshida-Ansari <anshidaansari9a@gmail.com>
2025-06-14 17:42:10 +0530

    initial commit

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -3,7 +3,7 @@ import './Login.css'
 import logo from '../../assets/logo.png';
 import { login,signup } from '../../firebase';
 import netflix_spinner from '../../assets/netflix_spinner.gif';
-
+import { toast } from 'react-toastify';
 
 const Login = () => {
 
@@ -11,11 +11,43 @@ const [signState,setSignState] = useState('Sign In')
 const [name,setName] = useState("")
 const [email,setEmail] = useState("")
 const [password,setPassword] = useState("")
-  const [loading, setLoading] = useState(false);
+const [loading, setLoading] = useState(false);
+
+const validateForm = () => {
+    if (signState === 'Sign Up' && name.trim() === '') {
+      toast.error('Name is required');
+      return false;
+    }
+
+    if (!email.trim()) {
+      toast.error('Email is required');
+      return false;
+    }
+
+    const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!emailPattern.test(email)) {
+      toast.error('Invalid email format');
+      return false;
+    }
+
+    if (!password) {
+      toast.error('Password is required');
+      return false;
+    }
+
+    if (password.length < 6) {
+      toast.error('Password must be at least 6 characters long');
+      return false;
+    }
+
+    return true;
+  };
 
 
 const user_auth =async (event)=>{
   event.preventDefault()
+
+   if (!validateForm()) return;
   setLoading(true)
   if(signState === 'Sign In'){
     await login(email,password)
